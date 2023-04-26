22d27eb8589639798e33e0d841d42df6d543b69f
Kenobiben <dragomir.stankovic@gmail.com>
2023-04-26 17:52:45 +0200

    Initial commit
    Started lesson

---
+++ Semos-AdvancedCodeClass/Assets/Scripts/UnityClassCode.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityClassCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ispecati gi broevite od 1 do 10
+
+        //for (int i = 1; i < 10; i++)
+        //{
+        //    Debug.Log(i); 
+        //}
+
+        int age = 5; // deklaracija i inicijacija na promenliva
+        Debug.Log(age); // pecatenje na promenliva
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+    
+
+
+}
