b89896edc2c0f5dc2312784ed7f32175acc6cb44
Kenobiben <dragomir.stankovic@gmail.com>
2023-04-27 13:55:57 +0200

    Adding arrays exercise
    added class arrays exercises

--- Semos-AdvancedCodeClass/Assets/Scripts/UnityClassCode.cs
+++ Semos-AdvancedCodeClass/Assets/Scripts/UnityClassCode.cs
@@ -17,6 +17,22 @@ public class UnityClassCode : MonoBehaviour
         int age = 5; // deklaracija i inicijacija na promenliva
         Debug.Log(age); // pecatenje na promenliva
 
+        int[] ages1 = new int[] { 10, 11, 15, 20, 13, 3 }; // nacin na 1 inicijacija
+        int[] ages2 = new int[5]; // nacin na 2 inicijacija
+
+       Debug.Log(ages1[2] * ages2[2]);
+        
+        // So pomos na ciklus, ispecatete gi site elementi od dvete nizi. 
+
+        for (int i = 0; i < ages1.Length; i++)
+        {
+            Debug.Log(ages1[i]);
+        }
+        Debug.Log("");
+        for(int i = 0; i < ages2.Length; i++)
+        {
+            Debug.Log(ages2[i]);
+        }
 
 
 
