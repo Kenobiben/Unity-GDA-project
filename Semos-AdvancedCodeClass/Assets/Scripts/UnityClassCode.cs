ed436ccc01b1ff495ba8efb008c6bf7d6b7b26c2
Kenobiben <dragomir.stankovic@gmail.com>
2023-04-27 14:50:12 +0200

    task 03
    upload task 3

--- Semos-AdvancedCodeClass/Assets/Scripts/UnityClassCode.cs
+++ Semos-AdvancedCodeClass/Assets/Scripts/UnityClassCode.cs
@@ -14,26 +14,53 @@ public class UnityClassCode : MonoBehaviour
         //    Debug.Log(i); 
         //}
 
-        int age = 5; // deklaracija i inicijacija na promenliva
-        Debug.Log(age); // pecatenje na promenliva
+        // int age = 5; // deklaracija i inicijacija na promenliva
+        // Debug.Log(age); // pecatenje na promenliva
 
         int[] ages1 = new int[] { 10, 11, 15, 20, 13, 3 }; // nacin na 1 inicijacija
-        int[] ages2 = new int[5]; // nacin na 2 inicijacija
+        //int[] ages2 = new int[5]; // nacin na 2 inicijacija
 
-       Debug.Log(ages1[2] * ages2[2]);
-        
-        // So pomos na ciklus, ispecatete gi site elementi od dvete nizi. 
+        //Debug.Log(ages1[2] * ages2[2]);
 
-        for (int i = 0; i < ages1.Length; i++)
+        // // So pomos na ciklus, ispecatete gi site elementi od dvete nizi. 
+
+        // for (int i = 0; i < ages1.Length; i++)
+        // {
+        //     Debug.Log(ages1[i]);
+        // }
+        // Debug.Log("");
+        // for(int i = 0; i < ages2.Length; i++)
+        // {
+        //     Debug.Log(ages2[i]);
+        // }
+
+        // 2. DA gi ispecatime site elemeti od niza vo obraten redosled
+
+        //for (int i = ages1.Length -1; i >= 0 ; i--)
+        //{
+        //    Debug.Log(ages1[i]);
+        //}
+
+        //3. Kreirajte nova niza ages3
+        int[] ages3 = new int[ages1.Length];
+        // a. i ispolte ja so istite elementi kako nizata ages1
+        // b. so kvadratite od elementit od nizata ages1 -> 100, 121, 225, 400, 169, 9 
+        // c. presmetaj ja sumata na elementite od a i b
+
+        for (int i = 0; i < ages3.Length; i++)
         {
-            Debug.Log(ages1[i]);
+            ages3[i] = ages1[i] * ages1[i];
+            Debug.Log(ages3[i]);
         }
-        Debug.Log("");
-        for(int i = 0; i < ages2.Length; i++)
-        {
-            Debug.Log(ages2[i]);
-        }
+        int suma1 = 0;
+        int suma2 = 0;
 
+        for (int i = 0; i < ages3.Length; i++)
+        {
+            suma1 += ages1[i];
+            suma2 += ages3[i];
+        }
+        Debug.Log($"{suma1} { suma2}");
 
 
 
