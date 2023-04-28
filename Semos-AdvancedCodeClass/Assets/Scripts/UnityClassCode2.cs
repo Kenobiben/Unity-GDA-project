507010eb9ee2ae0d4c466666550482179d2bc5ce
Kenobiben <dragomir.stankovic@gmail.com>
2023-04-28 15:44:30 +0200

    Working on Class2 script
    Script working

--- Semos-AdvancedCodeClass/Assets/Scripts/UnityClassCode2.cs
+++ Semos-AdvancedCodeClass/Assets/Scripts/UnityClassCode2.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class UnityClassCode2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // kako da kreiram int promenliva
+    public int neznam = 0; // promenlivi  koi se del od klasata
+    public string name;
+    public char character;
+    public float decimalNumber;
+    public double doubleNumber;
+    public int[] array;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    private void Start()
     {
-        
+        int znam = 1; // lokalni promenlivi
+                //Debug.Log(array)
+
     }
+    
+        
+
+
+    
+
+    
 }
